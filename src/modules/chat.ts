c56fe88cdad7a3e0295f785914cf0a406c12af7e
gabriel silva <gabrielturing@gmail.com>
2022-02-28 02:31:17 -0300

    chat game

--- src/modules/chat.ts
+++ src/modules/chat.ts
@@ -1,40 +1,48 @@
-import { AnyChannel, Client, TextChannel } from 'discord.js';
-import socketio from 'socket.io-client';
-import Environment from '../core/environment';
-import { IChatData } from './chat.dto';
+import { AnyChannel, Client, TextChannel } from "discord.js";
+import socketio from "socket.io-client";
+import Environment from "../core/environment";
+import { IChatData } from "./chat.dto";
 
 export default class Chat {
-    private static client: Client;
+  private static client: Client;
 
-    public static async setClient(client: Client): Promise<void> {
-        this.client = client;
-    }
+  public static async setClient(client: Client): Promise<void> {
+    this.client = client;
+  }
 
-    public static async sendChat(): Promise<void> {
-        const channel: TextChannel | any = this.client.channels.cache.find((channel: AnyChannel): TextChannel | any => {
-            if (!channel.isText())
-                return;
+  public static async sendChat(): Promise<void> {
+    const channel: TextChannel | any = this.client.channels.cache.find(
+      (channel: AnyChannel): TextChannel | any => {
+        if (!channel.isText()) return;
 
-            const textChannel = channel as TextChannel;
+        const textChannel = channel as TextChannel;
 
-            if (textChannel.name !== '📄chat-ingame')
-                return;
+        if (textChannel.name !== "📄chat-ingame") return;
 
-            return textChannel;
-        });
+        return textChannel;
+      }
+    );
 
-        if (!channel)
-            return;
+    if (!channel) return;
 
-        const socket = socketio(`${Environment.get('SOCKETIO')}`);
+    //delete all messages in channel
+    /*channel.messages.fetch({ limit: 100 }).then((messages: any): void => {
+      messages.forEach((message: any): void => {
+        message.delete();
+      });
+    });*/
 
-        socket.on('messages', (data: IChatData[]): void => {
-            data.forEach((message: IChatData): void => {
-                if (message.message[0] !== '@')
-                    return;
-                //send message
-                channel.send(`${message.nick}: ${message.message}`);
-            });
-        });
-    }
+    const socket = socketio(`${Environment.get("SOCKETIO")}`);
+
+    socket.on("botmessages", (data: IChatData): void => {
+      if (data) {
+        if (data.message && data.nick) {
+          if (data!.message[0] === "@") {
+            channel.send(`${data!.nick}: ${data!.message}`);
+          }
+        }
+        socket.emit("botmessages", { id: data.id });
+      }
+    });
+  }
 }
