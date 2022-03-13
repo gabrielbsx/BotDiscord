e408d81e3cbba1520c42fa70ba96328e3f7d68fd
Gabriel <gabrielturing@gmail.com>
2022-03-13 15:06:58 -0300

    news

---
+++ src/modules/news/news.dto.ts
@@ -0,0 +1,10 @@
+export interface INews {
+    id: number;
+    thumbnail: string;
+    slug: string;
+    title: string;
+    content: string;
+    category: string;
+    created_at: Date;
+    update_at: Date;
+}
--- src/modules/news/news.ts
+++ src/modules/news/news.ts
@@ -1,13 +1,61 @@
-import { Client } from 'discord.js';
+import { AnyChannel, Client, MessageEmbed, TextChannel } from 'discord.js';
+import socketio from 'socket.io-client';
+import Environment from '../../core/environment/environment';
 
 export class News {
     private static client: Client;
+    private static categories = {
+        '1': '📰novidade',
+        '2': '🔧manutenção',
+        '3': '🩹patchlist',
+        '4': '📜nota',
+        '5': '📅evento',
+    };
 
     public static async setClient(client: Client): Promise<void> {
         this.client = client;
     }
 
     public static async start(): Promise<void> {
-        
+        console.log('News module started');
+
+        const socket = socketio(`${Environment.get('SOCKETIO')}`);
+
+        socket.on('news', (data: any): void => {
+            const channel: TextChannel | any = this.client.channels.cache.find(
+                (channel: AnyChannel): TextChannel | any => {
+                    if (!channel.isText()) return;
+
+                    const textChannel = channel as TextChannel;
+
+                    const category = this.categories[data.category];
+
+                    if (textChannel.name !== category) {
+                        return;
+                    }
+
+                    return textChannel;
+                }
+            );
+
+            if (!channel) {
+                return;
+            }
+
+            const embed = new MessageEmbed();
+
+            embed.setTitle(data.title);
+            embed.setThumbnail(data.thumbnail);
+            embed.setDescription(data.content);
+            embed.setColor('#0099ff');
+            embed.setURL(`https://wydunderworld.com/news/${data.slug}`);
+            embed.setFooter({
+                text: 'Made with ❤️ by Underworld BOT',
+            });
+            embed.setTimestamp();
+            embed.setImage(data.thumbnail);
+
+            channel.send(embed);
+        });
     }
 }
